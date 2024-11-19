b04b7dc244e2e5b50c630c1a37bd4a416cf05435
Maximo <81933239+antiguamaximo@users.noreply.github.com>
2024-11-18 19:56:55 -0500

    Finished HW

--- Class/111824/Tutorial 9 Data Files/tny_script.js
+++ Class/111824/Tutorial 9 Data Files/tny_script.js
@@ -8,19 +8,37 @@
    Date:   111824
 
 */
-/*Store the current date and time */
-var currentDay = new Date("Nov 18, 2024 7:10:50"); 
-var dateStr = currentDay.toLocaleDateString();
-var timeStr = currentDay.toLocaleTimeString();
+/* Execute the function to run and display the countdown clock */
+runClock();
+setInterval("runClock()", 1000);
+/*Function to craete and run the countdown clock */
+function runClock() {
 
-/* Display the current date and time */
-document.getElementById("dateNow").innerHTML = 
-dateStr +"<br>"+ timeStr;
+   /*Store the current date and time */
+   var currentDay = new Date(); 
+   var dateStr = currentDay.toLocaleDateString();
+   var timeStr = currentDay.toLocaleTimeString();
 
-window.alert("Welcome to Tulsa");
+   /* Display the current date and time */
+   document.getElementById("dateNow").innerHTML = 
+   dateStr +"<br>"+ timeStr;
 
-/*Display the time left until New Year's Eve */
-document.getElementById("days").textContent = "dd";
-document.getElementById("hrs").textContent = "hh";
-document.getElementById("mins").textContent = "mm";
-document.getElementById("secs").textContent = "ss";
+   window.alert("Welcome to Tulsa");
+   /*Calculate the days until January 1st */
+   var newYear = new Date("January 1, 2025");
+   var nextYear = currentDay.getFullYear()+ 1;
+   newYear.setFullYear(nextYear);
+   var daysLeft = (newYear - currentDay)/(1000*60*60*24);
+
+   /*Display the time left until New Year's Eve */
+   document.getElementById("days").textContent = Math.floor(daysLeft);
+   /*Calculate  the hours left in the current day*/
+   var hrsLeft =(daysLeft - Math.floor(daysLeft))*24;
+   document.getElementById("hrs").textContent = Math.floor(hrsLeft);
+
+   /* Calculate the minutes and seconds left in the current hour */
+   var minsLeft = (hrsLeft - Math.floor(hrsLeft))*60;
+   var secsLeft = (minsLeft - Math.floor(minsLeft))*60;
+   document.getElementById("mins").textContent = Math.floor(minsLeft);
+   document.getElementById("secs").textContent = Math.floor(secsLeft);
+}
