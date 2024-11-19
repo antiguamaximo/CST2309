95ddd8690252a5b0578767d8bc6359f1d502e027
Maximo <81933239+antiguamaximo@users.noreply.github.com>
2024-11-18 19:22:22 -0500

    Finished Labs

--- Class/111824/Tutorial 9 Data Files/tny_script.js
+++ Class/111824/Tutorial 9 Data Files/tny_script.js
@@ -4,8 +4,23 @@
    Tutorial Case
 
    Countdown Clock
-   Author: 
-   Date:   
+   Author: Maximo Antigua  
+   Date:   111824
 
 */
+/*Store the current date and time */
+var currentDay = new Date("Nov 18, 2024 7:10:50"); 
+var dateStr = currentDay.toLocaleDateString();
+var timeStr = currentDay.toLocaleTimeString();
 
+/* Display the current date and time */
+document.getElementById("dateNow").innerHTML = 
+dateStr +"<br>"+ timeStr;
+
+window.alert("Welcome to Tulsa");
+
+/*Display the time left until New Year's Eve */
+document.getElementById("days").textContent = "dd";
+document.getElementById("hrs").textContent = "hh";
+document.getElementById("mins").textContent = "mm";
+document.getElementById("secs").textContent = "ss";
